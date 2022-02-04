5cf757ea89555c0034ae166876041be2fecf24c4
suddiyo <suddii01@sju.ac.kr>
2022-02-04 23:03:51 +0900

    Chapter 04 - mustache로 화면 구성

--- src/main/java/com/suddiyo/springboot/domain/posts/PostsRepository.java
+++ src/main/java/com/suddiyo/springboot/domain/posts/PostsRepository.java
@@ -1,9 +1,16 @@
 package com.suddiyo.springboot.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 /*
 Entity 클래스와 기본 Entity Repository는 함께 위치해야 한다
  */
 public interface PostsRepository extends JpaRepository<Posts, Long> {    //<Entity 클래스, PK 타입>
+
+    @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
+    List<Posts> findAllDesc();
+
 }
--- src/main/java/com/suddiyo/springboot/service/posts/PostsService.java
+++ src/main/java/com/suddiyo/springboot/service/posts/PostsService.java
@@ -2,13 +2,17 @@ package com.suddiyo.springboot.service.posts;
 
 import com.suddiyo.springboot.domain.posts.Posts;
 import com.suddiyo.springboot.domain.posts.PostsRepository;
+import com.suddiyo.springboot.web.dto.PostsListResponseDto;
 import com.suddiyo.springboot.web.dto.PostsResponseDto;
 import com.suddiyo.springboot.web.dto.PostsSaveRequestDto;
 import com.suddiyo.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import javax.transaction.Transactional;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -21,15 +25,29 @@ public class PostsService {
     }
 
     @Transactional
-    public Long update(Long id, PostsUpdateRequestDto requestDto){
-        Posts posts = postsRepository.findById(id).orElseThrow(()-> new IllegalArgumentException("해당 게시글이 없습니다. id = "+id));
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
         posts.update(requestDto.getTitle(), requestDto.getContent());
         return id;
     }
 
-    public PostsResponseDto findById (Long id){
-        Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id = "+id));
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
         return new PostsResponseDto(entity);
     }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
+        postsRepository.delete(posts);
+    }
+
 }
 // 쿼리를 날린다는 것??? 뭘 말하는 것? update 부분에서는 쿼리를 안 날린다는데.
